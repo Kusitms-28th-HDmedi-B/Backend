27b1ce3d655539830f7ce6480a6cef3e7af786b5
hojeong2747 <hojeong2747@gmail.com>
2023-09-02 04:26:50 +0900

    feat(#1): 프로젝트 생성

---
+++ src/test/java/kusitms/hdmedi/HdmediApplicationTests.java
@@ -0,0 +1,13 @@
+package kusitms.hdmedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdmediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
