f9068b4efb022b1171598106f5a7ef6b178757a4
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-09-11 20:57:46 +0900

    fix(#13): 공지사항 api 경로 수정 및 swagger 연동
    * feat(#13): 공지사항 CRUD 구현
    
    * fix(#13): 공지사항 한 페이지당 5개씩 불러오도록 수정
    
    * fix(#13): 공지사항 여러개 API 반환시 필요없는 정보 제거
    
    * fix(#13): 공지사항 리스트를 API로 반환할 때 최신순으로 정렬하도록 수정
    
    * test(#13): 공지사항 테스트 코드 작성
    
    * fix(#13): 공지사항 API swagger 연동
    
    * fix(#13): 테스트 코드 오류 수정

--- src/main/java/kusitms/hdmedi/controller/announcement/AnnouncementController.java
+++ src/main/java/kusitms/hdmedi/controller/announcement/AnnouncementController.java
@@ -1,7 +1,16 @@
 package kusitms.hdmedi.controller.announcement;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kusitms.hdmedi.dto.request.announcement.AnnouncementRequest;
 import kusitms.hdmedi.dto.response.announcement.AnnouncementResponse;
+import kusitms.hdmedi.dto.response.news.NewsResponse;
 import kusitms.hdmedi.service.announcement.AnnouncementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -11,32 +20,46 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "announcement", description = "공지사항 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/announcement")
+@RequestMapping("/api/announcement")
 public class AnnouncementController {
     private final AnnouncementService announcementService;
 
+    @Operation(description = "모든 공지사항 조회하기", summary = "모든 공지사항 조회")
+    @ApiResponse(responseCode = "200", description = "OK",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = AnnouncementResponse.class))))
     @GetMapping("")
-    public List<AnnouncementResponse> getAll(@PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+    @Parameter(name = "page", description = "페이지 번호", in = ParameterIn.QUERY)
+    public List<AnnouncementResponse> getAll(@Parameter(hidden = true) @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         return announcementService.getAll(pageable);
     }
 
+    @Operation(description = "특정 공지사항 조회하기", summary = "특정 공지사항 조회")
+    @ApiResponse(responseCode = "200", description = "OK",
+            content = @Content(schema = @Schema(implementation = AnnouncementResponse.class)))
+    @Parameter(name = "announcementId", description = "공지사항 ID")
     @GetMapping("/{announcementId}")
     public AnnouncementResponse get(@PathVariable Long announcementId) {
         return announcementService.get(announcementId);
     }
 
+    @Operation(description = "공지사항 작성하기", summary = "공지사항 작성")
     @PostMapping("")
     public void create(@RequestBody AnnouncementRequest announcementRequest) {
         announcementService.create(announcementRequest);
     }
 
-    @PatchMapping("/{announcementId}")
+    @Operation(description = "공지사항 수정하기", summary = "공지사항 수정")
+    @Parameter(name = "announcementId", description = "공지사항 ID")
+    @PutMapping("/{announcementId}")
     public void update(@PathVariable Long announcementId, @RequestBody AnnouncementRequest announcementRequest) {
         announcementService.update(announcementId, announcementRequest);
     }
 
+    @Operation(description = "공지사항 삭제하기", summary = "공지사항 삭제")
+    @Parameter(name = "announcementId", description = "공지사항 ID")
     @DeleteMapping("/{announcementId}")
     public void delete(@PathVariable Long announcementId) {
         announcementService.delete(announcementId);
--- src/main/java/kusitms/hdmedi/controller/news/NewsController.java
+++ src/main/java/kusitms/hdmedi/controller/news/NewsController.java
@@ -3,6 +3,7 @@ package kusitms.hdmedi.controller.news;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -31,16 +32,18 @@ public class NewsController {
     private final NewsService newsService;
 
     @Operation(description = "모든 뉴스 조회하기", summary = "모든 뉴스 조회")
-    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = NewsResponse.class)))
+    @ApiResponse(responseCode = "200", description = "OK",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = NewsResponse.class))))
     @GetMapping("")
-    @Parameter(name = "page", description = "페이지 번호", example = "0", in = ParameterIn.QUERY)
+    @Parameter(name = "page", description = "페이지 번호", in = ParameterIn.QUERY)
     public List<NewsResponse> getAll(@Parameter(hidden = true) @PageableDefault(size = 7, sort = "publishedAt", direction = Sort.Direction.DESC) Pageable pageable) {
         return newsService.getAll(pageable);
     }
 
     @Operation(description = "특정 뉴스 조회하기", summary = "특정 뉴스 조회")
-    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = NewsResponse.class)))
-    @Parameter(name = "newsId", description = "뉴스 ID", example = "7")
+    @ApiResponse(responseCode = "200", description = "OK",
+            content = @Content(schema = @Schema(implementation = NewsResponse.class)))
+    @Parameter(name = "newsId", description = "뉴스 ID")
     @GetMapping("/{newsId}")
     public NewsResponse get(@PathVariable Long newsId) {
         return newsService.get(newsId);
@@ -53,14 +56,14 @@ public class NewsController {
     }
 
     @Operation(description = "뉴스 수정하기", summary = "뉴스 수정")
-    @Parameter(name = "newsId", description = "뉴스 ID", example = "7")
+    @Parameter(name = "newsId", description = "뉴스 ID")
     @PutMapping("/{newsId}")
     public void update(@PathVariable Long newsId, @RequestBody NewsRequest newsRequest) {
         newsService.update(newsId, newsRequest);
     }
 
     @Operation(description = "뉴스 삭제하기", summary = "뉴스 삭제")
-    @Parameter(name = "newsId", description = "뉴스 ID", example = "7")
+    @Parameter(name = "newsId", description = "뉴스 ID")
     @DeleteMapping("/{newsId}")
     public void delete(@PathVariable Long newsId) {
         newsService.delete(newsId);
