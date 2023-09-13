b25d832385b8c17c7cf5c79d520283db21cb081d
hojeong2747 <hojeong2747@gmail.com>
2023-09-14 00:52:11 +0900

    add(#26): Spring Security Config 파일들 추가

--- src/main/java/kusitms/hdmedi/config/JwtTokenFilter.java
+++ src/main/java/kusitms/hdmedi/config/JwtTokenFilter.java
@@ -1,2 +1,66 @@
-package kusitms.hdmedi.config;public class JwtTokenFilter {
+package kusitms.hdmedi.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        // authorizationHeader 비어있으면 로그인 X
+        if(authorizationHeader == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // authorizationHeader 값이 'Bearer '로 시작 안 하면 X
+        if(!authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 전송받은 값에서 'Bearer ' 뒷부분(JWT) 추출
+        String token = authorizationHeader.split(" ")[1];
+
+        // JWT 만료 시 인증 X
+        if(JwtTokenUtil.isExpired(token, secretKey)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // JWT loginId 추출
+        String loginId = JwtTokenUtil.getLoginId(token, secretKey).get();
+
+        // 추출한 loginId 로 User 찾아오기
+        User loginUser = userService.getLoginUserByLoginId(loginId);
+
+        // loginUser 정보로 UsernamePasswordAuthenticationToken 발급
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginUser.getLoginId(), null, List.of(new SimpleGrantedAuthority(loginUser.getRole().name())));
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        // 권한 부여
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+
 }
+
