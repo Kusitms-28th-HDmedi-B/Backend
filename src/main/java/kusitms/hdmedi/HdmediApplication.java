88f8ac4c15f24947239873d5013c8461e2138f58
nohy6630 <nohy6630@gmail.com>
2023-09-14 22:46:43 +0900

    fix(#32): swagger에서 http로 요청을 보내는 오류 수정

--- src/main/java/kusitms/hdmedi/HdmediApplication.java
+++ src/main/java/kusitms/hdmedi/HdmediApplication.java
@@ -1,8 +1,10 @@
 package kusitms.hdmedi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server(url = "https://hdmedi.site")})
 @SpringBootApplication
 public class HdmediApplication {
 
