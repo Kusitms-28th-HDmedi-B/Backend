2770d4fcda133400c451595047a25dcf60400e81
nohy6630 <129354455+nohy6630@users.noreply.github.com>
2023-09-14 16:17:20 +0900

    fix(#30): cors 설정 (#31)
    * fix(#30): api get 요청시에는 jwt 토큰 필요없도록 수정
    
    * fix(#30): cors 설정

--- src/main/java/kusitms/hdmedi/config/SecurityConfig.java
+++ src/main/java/kusitms/hdmedi/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import kusitms.hdmedi.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -50,6 +51,7 @@ public class SecurityConfig {
                                 .requestMatchers(mvcMatcherBuilder.pattern("/swagger-resources/**")).permitAll()
                                 .requestMatchers(mvcMatcherBuilder.pattern("/v3/api-docs/**")).permitAll()
                                 .requestMatchers(mvcMatcherBuilder.pattern("/auth/admin/**")).hasAuthority(UserRole.ADMIN.name())
+                                .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET,"/api/**")).permitAll()
                                 .anyRequest().authenticated())
                 .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class);;
 
@@ -67,11 +69,9 @@ public class SecurityConfig {
     // https://hdmedi.site/swagger-ui/index.html#/
     private CorsConfiguration getDefaultCorsConfiguration() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:8080", "https://hdmedi.site", "http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));// 모든 ip 응답을 허용
         configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 header 에 응답을 허용
         configuration.setAllowedMethods(Arrays.asList("*")); // 모든 get,post,patch,put,delete 요청 허용
-        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 ip 응답을 허용
         configuration.setAllowCredentials(true); // 내 서버가 응답할 때 json 을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
         configuration.setMaxAge(3600L);
 
