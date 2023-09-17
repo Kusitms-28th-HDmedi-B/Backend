0b4d0dd9bf08a3f2d5889a79405902d4e54a4cbc
nohy6630 <nohy6630@gmail.com>
2023-09-17 16:02:08 +0900

    fix(#37): 공지사항 content 데이터에 긴 문자열도 저장되게 수정

--- src/main/java/kusitms/hdmedi/domain/announcement/Announcement.java
+++ src/main/java/kusitms/hdmedi/domain/announcement/Announcement.java
@@ -1,9 +1,6 @@
 package kusitms.hdmedi.domain.announcement;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +20,8 @@ public class Announcement {
 
     private String title;
 
+    @Lob
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     @CreationTimestamp
